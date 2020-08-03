98a7de9259ecbbc5a4d37e2ab33d8a7193d3cfbb
lazalaki <lakierbes@gmail.com>
2020-08-03 12:48:03 +0200

    Extracting Controller Queries to the Model

--- app/Activity.php
+++ app/Activity.php
@@ -12,4 +12,16 @@ class Activity extends Model
     {
         return $this->morphTo();//sam ce razumeti na sta se subject odnosi
     }
+
+    public static function feed($user, $take = 50)
+    {
+        return $user->activity()
+            ->latest()
+            ->with('subject')
+            ->take($take)
+            ->get()
+            ->groupBy(function ($activity) {
+                return $activity->created_at->format('Y-m-d');
+            });
+    }
 }
